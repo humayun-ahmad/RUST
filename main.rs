d15a56a6ddfc2ce046ebd9cdc6ce809d493c0be3
humayun-ahmad <humayunraj789@gmail.com>
2024-03-04 10:04:59 +0600

    Correction

--- main.rs
+++ main.rs
@@ -1,6 +1,19 @@
-use clap::ColorChoice;
+
+// Fix the error below with least amount of modification to the code
+fn main() {
+    let x: i32; // Uninitialized but used, ERROR !
+    let y: i32; // Uninitialized but also unused, only a Warning !
+
+    assert_eq!(x, 5);
+    println!("Success!");
+}
 
 
-fn main(){
-    println!("Hello world");
+// Fill the blanks in the code to make it compile
+fn demo1() {
+    let __ __ = 1;
+    __ += 2; 
+    
+    assert_eq!(x, 3);
+    println!("Success!");
 }
