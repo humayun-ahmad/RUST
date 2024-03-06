639c8a83b4fd4e47f683457c2931a0de52871e7e
humayun-ahmad <humayunraj789@gmail.com>
2024-03-06 11:31:10 +0600

    Ownership Rules

---
+++ ownership.rs
@@ -0,0 +1,4 @@
+// Ownership Rules:
+// Each value in the rust has a variable that is its owner.
+// A value can have only owner at a time
+// When the owner goes out of scope, the value is dropped.
