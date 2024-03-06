adf9ccc74ccc3883114488a1ea200661292ea3ca
humayun-ahmad <humayunraj789@gmail.com>
2024-03-06 15:55:40 +0600

    string procedure

--- ownership.rs
+++ ownership.rs
@@ -41,9 +41,35 @@ fn main(){
     println!("{}", heap_i8_2);
 
 
+    let stack_f64: f64 = 1.;
+    let heap_f64: Box<f64> = Box::new(2.);
 
+    
+    stack_procedure(&stack_f64);
+    println!("In main stack {}", stack_f64);
+    
+    heap_procedure(&heap_f64);
+    println!("In main heap {}", heap_f64);
 
+    let some_string: String = String::from("Rajib"); // String are always on the heap
+    let some_str: &str = "Partner"; // &str is a pointer... to either stack or heap
+    
+    some_procedure(&some_string, some_str);
 
-
+    println!("{} {}", some_string, some_str);
 
 }
+
+
+fn stack_procedure(param: &f64){
+    println!("In stack_procedure with param {}", param);
+}
+
+fn heap_procedure(param: &Box<f64>){
+    println!("In heap_procedure with param {}", param);
+}
+
+fn some_procedure(param_a: &String, param_b: &str){
+    println!("In heap_procedure with param {} {}", param_a, param_b);
+}
+
