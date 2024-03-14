f2d5c48e6f6352df8358615480999a8be07fb98e
humayun-ahmad <humayunraj789@gmail.com>
2024-03-14 14:28:36 +0600

    Excercise from the book

--- Ownershipt_fcc.rs
+++ Ownershipt_fcc.rs
@@ -1,1 +1,25 @@
 // Understanding Ownership
+
+fn main() {
+    // let s = "hello";
+    let mut s = String::from("Hello");
+    
+    // push_str() appends a literal to a String
+    s.push_str(", World!");
+    
+    println!("{}", s);
+    
+    
+    let s1 = String::from("hello");
+    
+    let s2 = s1.clone();
+    
+    println!("{}",s2);
+    
+    
+    let x = 5;
+    let y = x;
+
+    println!("x = {}, y = {}", x, y);
+    
+}
