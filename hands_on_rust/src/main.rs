a63b2feb06d1199c6657d19cc6c653ccb32d739a
humayun-ahmad <humayunraj789@gmail.com>
2024-03-15 21:26:17 +0600

    Enum practice

--- hands_on_rust/src/main.rs
+++ hands_on_rust/src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
-    println!("Hello, world!");
+    example_enum_with_data();
+}
+
+
+enum Address{
+    Number(i32),
+    Name(String),
+    Unknown
+}
+
+
+fn example_enum_with_data(){
+    println!("\nexample enums with data");
+    let h: Address = Address::Number(4);
+
+    match h {
+        Address::Number(n) => println!("You live in house number {}", n),
+        Address::Name(s) => println!("You live in a house named {}", s),
+        Address::Unknown => println!("Your house location is unknown"),
+    }
+
+    println!("Btw the size of Address is {} bytes", std::mem::size_of::<Address>());
 }
