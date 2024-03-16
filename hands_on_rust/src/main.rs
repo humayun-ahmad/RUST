499198e1ea1de9b90e9893a276e318b0523cf6c9
humayun-ahmad <humayunraj789@gmail.com>
2024-03-16 12:20:54 +0600

    vector practice

--- hands_on_rust/src/main.rs
+++ hands_on_rust/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     // example_enum_with_data();
-    example_enum_with_function();
+    // example_enum_with_function();
+    example_vec();
 }
 
 
@@ -32,5 +33,26 @@ fn example_enum_with_function(){
     match favnum {
         Some(n) => println!("My favorite number is {}, good choice", n),
         None => println!("What is the fuchka?"),
+    }
+}
+
+fn example_vec(){
+    println!("Using a vector");
+
+    let mut vec1: Vec<i32> = Vec::new();
+    let mut _vec1b=Vec::<i32>::new();
+
+    vec1.push(20);
+    vec1.push(101);
+    vec1.push(100);
+
+    println!("vec1 is {:?}, length is {}, first element is {}", vec1, vec1.len(), vec1[0]);
+
+    let mut vec2 = vec!["Dhaka", "Barisal", "khulna"];
+
+    vec2.insert(0, "Rajshahi");
+
+    for item in vec2 {
+        println!("{}", item);
     }
 }
