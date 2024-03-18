148756c1328ec312a006472b0882592084d2a5b3
humayun-ahmad <humayunraj789@gmail.com>
2024-03-18 14:24:23 +0600

    Return has beenused as User

---
+++ structs_types_enums/target/debug/deps/structs_types_enums.d
@@ -0,0 +1,5 @@
+C:\Users\Rajib\Documents\Rust + Substrate\RUST\structs_types_enums\target\debug\deps\structs_types_enums.exe: src\main.rs
+
+C:\Users\Rajib\Documents\Rust + Substrate\RUST\structs_types_enums\target\debug\deps\structs_types_enums.d: src\main.rs
+
+src\main.rs:
---
+++ structs_types_enums/target/debug/structs_types_enums.d
@@ -0,0 +1,1 @@
+C:\Users\Rajib\Documents\Rust\ +\ Substrate\RUST\structs_types_enums\target\debug\structs_types_enums.exe: C:\Users\Rajib\Documents\Rust\ +\ Substrate\RUST\structs_types_enums\src\main.rs
