a7ee71133926509bae8646b1796698eedb9a1296
humayun-ahmad <humayunraj789@gmail.com>
2024-03-19 12:27:41 +0600

    vector retrieving

--- string_vector/src/main.rs
+++ string_vector/src/main.rs
@@ -1,5 +1,36 @@
 use std::any::type_name_of_val;
 
+
+fn get_item(index: usize) {
+    //let index = 3; // this looks like an unsigned integer, but it's actually a usize
+    let vec = vec![1, 2, 3, 4, 5];
+
+    // Retrieve a value at a specific index
+    let value = vec.get(index).unwrap();
+
+    // print the value
+    println!("The value at index {} is {:?}", index, value);
+}
+
+fn retrieving_value_from_vector(){
+    let vec = vec![1,2,3,4,5];
+
+    // Retrieve a value at a specific index
+    let third_value = vec[2];
+    println!("The third value in the vector is : {}", third_value);
+
+    // Retrieve the last value
+    let last_value = vec.last().unwrap();
+    println!("The last value in the vector is : {}", last_value);
+
+    //Retrieve the first value using pattern matching
+    match vec.first() {
+        Some(first_value) => println!("The first value in the vector is: {}\n", first_value),
+        None => println!("The vector is empty!\n"),
+    }
+}
+
+#[warn(dead_code)]
 fn variable_type(){
     let sentence = "the quick brown fox jumps over the lazy dog";
     println!("{}", type_name_of_val(&sentence));
@@ -58,7 +89,10 @@ fn main() {
 
     // variable_type();
     let sentence = "the quick brown fox jumps over the lazy dog";
-    string_slice(sentence);
+    // string_slice(sentence);
+
+    let vec = vec![1, 2, 3, 4, 5];
+    get_item(3);
 
     println!("Execution Compeleted!");
 }
