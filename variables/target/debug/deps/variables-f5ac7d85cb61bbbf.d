6e94ea50c8c154665c45e8c1ceca5a3a743d960a
humayun-ahmad <humayun.ahmad.rajib@gmail.com>
2024-03-28 13:53:39 +0600

    variables

---
+++ variables/target/debug/deps/variables-f5ac7d85cb61bbbf.d
@@ -0,0 +1,5 @@
+/home/rajib/Documents/My work/RUST/variables/target/debug/deps/libvariables-f5ac7d85cb61bbbf.rmeta: src/main.rs
+
+/home/rajib/Documents/My work/RUST/variables/target/debug/deps/variables-f5ac7d85cb61bbbf.d: src/main.rs
+
+src/main.rs:
